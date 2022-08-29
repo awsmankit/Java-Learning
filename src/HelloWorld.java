616bc94c478966e1b128f9991d56554cf0fa5e6a
Ankit <iamankit497@gmail.com>
2022-08-30 01:20:25 +0530

    Adding more projects to this folder

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
+
 class HelloWorld
 {
     public static void main(String[] args)
     {
         System.out.println("Welcome 2 Java");
         System.out.println("Hello");
-        
     }
 }
